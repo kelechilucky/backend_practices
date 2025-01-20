68fc82ad209cbf52d0e29b543e889778204485d9
Ikeolu Kelechi Lucky <kelechiikeolu@gmail.com>
2025-01-20 09:41:13 +0100

    FEAT: modified the user schema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,24 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema({})
+const UserSchema = new mongoose.Schema({
+    Username:{
+        type:String,
+        required: true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:String,
+        required:true,
+        match:[/^\d{10,15$}/, "phone number must be 10 to 15 digits"],
+    },
+    phone:{
+        type:String,
+        required:true,
+        match:[/^\S{10,15$}/, "phone number must be 10 to 15 digits"],
+    }
+})
