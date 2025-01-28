69d9faa40df99aa5ae697007e768a60c709e0e58
Ikeolu Kelechi Lucky <kelechiikeolu@gmail.com>
2025-01-28 02:24:21 +0100

    FEAT: Completed user schema

--- models/User.js
+++ models/User.js
@@ -35,3 +35,5 @@ UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
